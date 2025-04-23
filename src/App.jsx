8a020c1e552e66ee2b9c67b70fcf93fdceb45489
jaehyun <leejh3940@skuniv.ac.kr>
2025-04-23 09:32:31 +0900

    0423

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import MainPage from "./pages/MainPage.jsx";
 import SearchResult from "./pages/SearchResult.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,8 +5,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 export function App() {
   const router = createBrowserRouter([
 
-    { path: "/MainPage", element: <MainPage />}
-    { path: "/search", element: <SearchResult/>} //검색 결과 화면
+    { path: "/MainPage", element: <MainPage />},
+    { path: "/search", element: <SearchResult/>}, //검색 결과 화면
   ]);
   return <RouterProvider router={router} />;
 }
