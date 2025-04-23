ec91709ee04b22bac72be7b5e72e57ba30a00213
lastcoder <hw0005@naver.com>
2025-04-23 13:16:29 +0900

    2025_04_23_(3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
 import MainPage from "./pages/MainPage.jsx";
 import SearchResult from "./pages/SearchResult.jsx";
+import SignUpPage from './pages/SignUpPage.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 export function App() {
   const router = createBrowserRouter([
 
-    { path: "/mainPage", element: <MainPage />},
+    { path: "/mainPage", element: <MainPage />},  // 메인 페이지 화면
     { path: "/search", element: <SearchResult/>}, //검색 결과 화면
+    { path: "/signUp", element: <SignUpPage />}, // 회원가입 화면
 
   ]);
   return <RouterProvider router={router} />;
