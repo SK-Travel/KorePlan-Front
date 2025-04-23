c195840bab15e62dffd52d200ccdd110f150c850
lastcoder <hw0005@naver.com>
2025-04-23 10:20:39 +0900

    2025_04_23_(2)

--- src/fragments/Header.jsx
+++ src/fragments/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // 부트스트랩 CSS 가져오기
+import KorePlanLogo from '../assets/KorePlan.png';
 
 const Header = () => {
     return (
         <header className="bg-success">
             <div className="h-100 d-flex justify-content-between align-items-center mx-4">
-                <img src="public/KorePlan.png" alt="KorePlan 로고" width="100" />
+                <img src={KorePlanLogo} alt="KorePlan 로고" width="100" />
                 <h1>KorePlan</h1>
                 <h1>현우님 안녕하세요</h1>
             </div>
