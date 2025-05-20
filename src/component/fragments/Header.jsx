fc54cb46eabecc23b9ce7d7c2fa4eb86c2398ef8
HYUNWOO\hw000 <hw0005@naver.com>
2025-05-20 14:38:06 +0900

    2025_05_20_Second

--- src/component/fragments/Header.jsx
+++ src/component/fragments/Header.jsx
@@ -16,13 +16,8 @@ import {
 import Logo from "../../assets/LogoIcon.png";
 
 const Header = () => {
-
   const navigate = useNavigate();
   const goToSearch = () => navigate("/search");
-
-  return (
-    <HeaderWrapper>
-
   const [show, setShow] = useState(false);
   const toggleMenu = () => setShow((prev) => !prev);
   const [userName, setUserName] = useState(null);
@@ -45,19 +40,6 @@ const Header = () => {
   }
   return (
     <HeaderWrapper>
-      <LogoWrapper>
-        {userName ? (
-          <Link to="/mainPage">
-            <LogoImage src={sam} alt="KorePlan 로고" />
-          </Link>
-        ) : (
-          <Link to="/signin">
-            <LogoImage src={sam} alt="KorePlan 로고" />
-          </Link>
-        )}
-      </LogoWrapper>
-
-
       {/* Logo + 텍스트 */}
       <LogoLink to="/mainPage">
         <LogoImage src={Logo} alt="KorePlan 로고" />
@@ -71,13 +53,6 @@ const Header = () => {
       {/* 우측 아이콘들 */}
       <UserSection>
 
-        {/* 검색 아이콘  /search 로 이동 */}
-        <Button onClick={goToSearch}>
-          <TbMapSearch style={{ fontSize: 45, color: "#000" }} />
-        </Button>
-
-        {/* MyList 메뉴창창 */}
-        <MyList />
 
         {userName ? (
         <>
@@ -85,44 +60,19 @@ const Header = () => {
             <a type="button" href="/infoModified" style={{ color: 'green', fontSize: '19px', textDecoration: 'none'}}>
               {userName}님 안녕하세요
             </a>
-            {/* 검색 버튼 */}
-            <Button onClick={toggleMenu}><TbMapSearch style={{ fontSize: '50px', color: '#000' }} /></Button>
-            {/* SwipeableDrawer */}
-            <SwipeableDrawer
-              anchor="top"
-              open={show}
-              onClose={toggleMenu}
-              onOpen={toggleMenu}
-              PaperProps={{
-                sx: {
-                  height: '50%',
-                }
-              }}
-            >
-              <Row>
-              <LogoWrapper>
-                <Link to="/mainPage">
-                  <LogoImage src={sam} alt="KorePlan 로고" />
-                </Link>
-              </LogoWrapper>
-              <SearchBarResponsive style={{marginTop: '20px'}} />
-              <RightSide/>
-              </Row>
-            </SwipeableDrawer>
+            {/* 검색 아이콘  /search 로 이동 */}
+            <Button onClick={goToSearch}>
+              <TbMapSearch style={{ fontSize: 45, color: "#000" }} />
+            </Button>
             <MyList />
-            {/* <div style={{ marginRight: '20px', gap: '10px'}}> */}
-              {/* <a href="#" onClick={handleLogOut} style={{ color: 'red', textDecoration: 'none'}}>
-                로그아웃
-              </a> */}
-            {/* </div> */}
         </>
         ) : (
         <>
           {/* 비로그인 시 */}
             <div style={{ marginRight: '20px' }}>
-              <Link to="/signin" style={{ color: 'green', fontSize: '18px', textDecoration: 'none' }}>
+              <LogoLink to="/signin" style={{ color: 'green', fontSize: '18px', textDecoration: 'none' }}>
                 로그인
-              </Link>
+              </LogoLink>
             </div>
         </>
         )}
