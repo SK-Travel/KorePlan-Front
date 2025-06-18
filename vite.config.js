c9a7c434e075d0ae1941002e87e0a3bbcb5c8375
jaehyun <leejh3940@skuniv.ac.kr>
2025-06-18 11:32:01 +0900

    dd

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://14.63.178.142:8080',
-        changeOrigin: true,
+export default defineConfig(({ command, mode }) => {
+  // 환경변수 로드
+  const env = loadEnv(mode, process.cwd(), '')
+  const apiBaseUrl = env.VITE_API_BASE_URL || 'http://localhost:8080'
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: apiBaseUrl,
+          changeOrigin: true,
+        },
+        '/oauth2': {
+          target: apiBaseUrl,
+          changeOrigin: true,
+        },
+        '/login/oauth2': {
+          target: apiBaseUrl,
+          changeOrigin: true,
+        }
       },
-      '/oauth2': {
-        target: 'http://14.63.178.142:8080',
-        changeOrigin: true,
-      },
-      '/login/oauth2': {
-        target: 'http://14.63.178.142:8080',
-        changeOrigin: true,
-      }
     },
-  },
+  }
 });
